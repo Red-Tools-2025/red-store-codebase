b07f71ae2905378e82c901b3829625f081495311
akshatsabavat <akshat.sabavat@gmail.com>
2024-10-12 03:22:50 +0530

    Defined: Basic static function for handling input changes

--- red-store-codebase/src/app/hooks/auth/StaticHooks/useLoginStaticHook.tsx
+++ red-store-codebase/src/app/hooks/auth/StaticHooks/useLoginStaticHook.tsx
@@ -1,5 +1,15 @@
+import { Dispatch, SetStateAction } from "react";
+
 const useLoginStaticHook = () => {
-  return;
+  // handleInput only for string feild types
+  const handleInputChange = (
+    inputState: string,
+    setInputState: Dispatch<SetStateAction<string>>
+  ) => {
+    setInputState(inputState);
+    console.log(inputState); // comment out later
+  };
+  return { handleInputChange };
 };
 
 export default useLoginStaticHook;
