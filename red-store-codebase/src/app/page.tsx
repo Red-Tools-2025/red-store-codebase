aa51bbc03d67b0f48345e52e4f9872b65aca51ae
akshatsabavat <akshat.sabavat@gmail.com>
2024-10-12 03:16:24 +0530

    Added: temporary tailwind layout format

--- red-store-codebase/src/app/page.tsx
+++ red-store-codebase/src/app/page.tsx
@@ -3,7 +3,7 @@ import TestLoginForm from "@/components/testing/TestLogin";
 const Home = () => {
   return (
     <div className="container mx-auto p-4">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col h-screen justify-center items-center">
         <TestLoginForm />
       </div>
     </div>
