36c60a14325f5561130e919cc3d7648bd17791d1
akshatsabavat <akshat.sabavat@gmail.com>
2024-10-12 11:00:17 +0530

    Chore: Installed type dependencies for bcrypt

---
+++ red-store-codebase/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+import { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
