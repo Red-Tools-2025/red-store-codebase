ef5a132d24b7cde56d852fdad08716d0e7a0eeb3
akshatsabavat <akshat.sabavat@gmail.com>
2024-10-12 11:10:20 +0530

    Defined: Password feild for schema, completed migrations to RDS

--- red-store-codebase/src/app/api/auth/[...nextauth]/route.ts
+++ red-store-codebase/src/app/api/auth/[...nextauth]/route.ts
@@ -3,3 +3,35 @@ import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", placeholder: "Enter Email", type: "text" },
+        password: { label: "password", placeholder: "*****", type: "text" },
+      },
+      async authorize(credentials) {
+        // Ensures that there is data in the form to check
+        if (!credentials?.email || !credentials.password) return null;
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        // unable to fetch user based on provided email, hence invalid or no account
+        if (!user) return null;
+
+        const isPasswordValid = await bcrypt.compare(
+          credentials.password,
+          user
+        );
+      },
+    }),
+  ],
+};
+
+export default authOptions;
