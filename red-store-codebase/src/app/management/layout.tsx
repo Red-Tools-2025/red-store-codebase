8cf332938561437aa93ba6b563ce0f779664ac2a
akshatsabavat <akshat.sabavat@gmail.com>
2024-11-01 22:38:14 +0530

    Defined: page CTAs

--- red-store-codebase/src/app/management/layout.tsx
+++ red-store-codebase/src/app/management/layout.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { IoMdPersonAdd } from "react-icons/io";
+import { IoStorefront } from "react-icons/io5";
+import { MdOutlineAssignment } from "react-icons/md";
 import React from "react";
 
 interface ManagementPageLayoutProps {
@@ -8,8 +12,25 @@ const ManagementPageLayout: React.FC<ManagementPageLayoutProps> = ({
   children,
 }) => {
   return (
-    <div>
-      <h1>Management Page Header</h1>
+    <div className="p-5">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold">User and Store Management</h1>
+        <div className="flex gap-2">
+          <Button>Choose Outlet</Button>
+          <Button variant={"icon-left"}>
+            <MdOutlineAssignment className="mr-2" />
+            Assign Employee
+          </Button>
+          <Button variant={"icon-left"}>
+            <IoMdPersonAdd className="mr-2" />
+            Add Employee
+          </Button>
+          <Button variant={"icon-left"}>
+            <IoStorefront className="mr-2" />
+            Add Store
+          </Button>
+        </div>
+      </div>
       {children}
     </div>
   );
