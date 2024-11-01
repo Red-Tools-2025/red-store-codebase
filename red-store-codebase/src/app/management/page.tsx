8ac5849eb06e6c370a1500ee7c872005817ede3a
akshatsabavat <akshat.sabavat@gmail.com>
2024-11-01 22:04:50 +0530

    Updated: session token to pass through session data

--- red-store-codebase/src/app/management/page.tsx
+++ red-store-codebase/src/app/management/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const ManagementPage: React.FC = () => {
+  const { data: session } = useSession();
+  console.log(session);
   return <main>Management page content goes here</main>;
 };
 
