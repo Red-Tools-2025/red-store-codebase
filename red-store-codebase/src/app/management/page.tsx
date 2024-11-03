0de6b308a6fa15f2b1e5be276def83c62e973ccc
akshatsabavat <akshat.sabavat@gmail.com>
2024-11-03 11:54:32 +0530

    Defined: seperate control pane for employee data

--- red-store-codebase/src/app/management/page.tsx
+++ red-store-codebase/src/app/management/page.tsx
@@ -2,6 +2,7 @@
 import { useManagement } from "../contexts/management/ManagementContext";
 import React from "react";
 import StoreControlPanel from "@/components/feature/management/feature-component/Panels/StoreControlPanel";
+import EmployeeControlPanel from "@/components/feature/management/feature-component/Panels/EmployeeConrolPanel";
 
 const ManagementPage: React.FC = () => {
   const { sessionData, storeData } = useManagement();
@@ -10,15 +11,8 @@ const ManagementPage: React.FC = () => {
       <div className="flex my-2 gap-2">
         {/* Control panel for store data */}
         <StoreControlPanel storeData={storeData} />
-        <div className="bg-green-300 flex w-1/2 p-4 flex-col">
-          <h1 className="text-lg font-semibold mb-2">User Data</h1>
-          {/* Render user-related data here */}
-          {sessionData ? (
-            <div>{/* Render session data here */}</div>
-          ) : (
-            <p className="text-center text-gray-700">No user data available</p>
-          )}
-        </div>
+        {/* Control panel for employee data */}
+        <EmployeeControlPanel />
       </div>
     </main>
   );
