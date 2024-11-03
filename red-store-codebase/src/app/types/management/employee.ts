0d699027d817d3e623e9984b0cd0d4885e795c59
akshatsabavat <akshat.sabavat@gmail.com>
2024-11-03 23:54:51 +0530

    Defined: Redundant emp type --to be changed later

--- red-store-codebase/src/app/types/management/employee.ts
+++ red-store-codebase/src/app/types/management/employee.ts
@@ -6,3 +6,12 @@ export interface AddEmployeeRequestBody {
   empStatus: boolean;
   storeManagerId: string; // This is used for partitioning
 }
+
+export interface employeeType {
+  storeId: number | string; // Allow both number and string to handle conversion
+  roleId: string;
+  empName: string;
+  empPhone: string;
+  empStatus: boolean;
+  storeManagerId: string; // This is used for partitioning
+}
