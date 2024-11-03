fb1a085fe7fc46539e3a13c091d763e9349adc55
akshatsabavat <akshat.sabavat@gmail.com>
2024-11-03 07:57:25 +0530

    Chore: defined layout for management page + added shadcn table ui componentd

--- red-store-codebase/src/app/management/page.tsx
+++ red-store-codebase/src/app/management/page.tsx
@@ -3,8 +3,15 @@ import React from "react";
 import { useManagement } from "../contexts/management/ManagementContext";
 
 const ManagementPage: React.FC = () => {
-  const { sessionData } = useManagement();
-  return <main>Management page content goes here</main>;
+  const { sessionData, storeData } = useManagement();
+  return (
+    <main>
+      <div className="flex my-2 gap-2">
+        <div className="bg-red-300 flex w-1/2">Stores data here</div>
+        <div className="bg-green-300 flex w-1/2">User data here</div>
+      </div>
+    </main>
+  );
 };
 
 export default ManagementPage;
