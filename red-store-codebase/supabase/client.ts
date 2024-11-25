0d59ebd4d7cf212d1bf71c4d2a5e1851979d656b
akshatsabavat <akshat.sabavat@gmail.com>
2024-11-25 07:11:16 +0530

    Added: DB type to supabase to ensure end to end typesafety

--- red-store-codebase/supabase/client.ts
+++ red-store-codebase/supabase/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_KEY, SUPABASE_URL } from "./env";
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+import { SUPABASE_KEY, SUPABASE_URL } from "./env";
+import { Database } from "./database.types";
+const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
 
 export default supabase;
