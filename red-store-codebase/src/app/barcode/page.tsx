091f055ed48434a1ea8a3d1c0746a903d53bdb43
akshatsabavat <akshat.sabavat@gmail.com>
2024-12-04 05:42:10 +0530

    Fixed: dynamsoft type issues --updated TextResult import

--- red-store-codebase/src/app/barcode/page.tsx
+++ red-store-codebase/src/app/barcode/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import BarcodeScanner from "@/components/BarcodeScanner";
-import { TextResult } from "dynamsoft-javascript-barcode/dist/types/interface/textresult";
+import { TextResult } from "dynamsoft-javascript-barcode";
 import Head from "next/head";
 import { Button } from "@/components/ui/button";
 
