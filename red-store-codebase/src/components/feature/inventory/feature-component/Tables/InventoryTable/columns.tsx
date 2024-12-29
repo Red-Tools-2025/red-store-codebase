6dcbcffe5be8059ed289c3ebe8fe52da040179c4
akshatsabavat <akshat.sabavat@gmail.com>
2024-12-29 18:04:21 +0530

    Defined: tanstack column definition

---
+++ red-store-codebase/src/components/feature/inventory/feature-component/Tables/InventoryTable/columns.tsx
@@ -0,0 +1,47 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { Inventory } from "@prisma/client";
+
+export const columns: ColumnDef<Inventory>[] = [
+  {
+    accessorKey: "invId",
+    header: "ID",
+    cell: ({ row }) => `#${row.getValue("invId")}`,
+  },
+  {
+    accessorKey: "invItem",
+    header: "Product",
+  },
+  {
+    accessorKey: "invItemBrand",
+    header: "Brand",
+  },
+  {
+    accessorKey: "invItemType",
+    header: "Type",
+  },
+  {
+    accessorKey: "invItemStock",
+    header: "Quantity",
+    cell: ({ row }) => row.getValue("invItemStock"),
+  },
+  {
+    accessorKey: "invItemPrice",
+    header: "Price",
+    cell: ({ row }) => `₹${row.getValue("invItemPrice")}`,
+  },
+  //   {
+  //     accessorKey: "invAdditional",
+  //     header: "Additional Info",
+  //     cell: ({ row }) => {
+  //       const additionalInfo = row.getValue("invAdditional") as Record<
+  //         string,
+  //         unknown
+  //       >;
+  //       return additionalInfo
+  //         ? `Size: ${additionalInfo.size ?? ""}, Measurement: ${
+  //             additionalInfo.measurement ?? ""
+  //           }`
+  //         : "N/A";
+  //     },
+  //   },
+];
