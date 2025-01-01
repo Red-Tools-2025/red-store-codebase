26fa7d09c39763837c3254900db9c47449f0c0f7
akshatsabavat <akshat.sabavat@gmail.com>
2025-01-01 20:45:33 +0530

    Created: empty state

--- red-store-codebase/src/components/feature/inventory/feature-component/Tables/InventoryDataTable/index.tsx
+++ red-store-codebase/src/components/feature/inventory/feature-component/Tables/InventoryDataTable/index.tsx
@@ -1,15 +1,7 @@
-import React, { useState } from "react";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  SortingState,
-  Table as TableType,
-  useReactTable,
-} from "@tanstack/react-table";
+import React from "react";
+import { flexRender, Table as TableType } from "@tanstack/react-table";
 import { motion, AnimatePresence } from "framer-motion";
-
+import { GiEmptyMetalBucketHandle } from "react-icons/gi";
 import {
   Table,
   TableBody,
@@ -19,7 +11,6 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Inventory } from "@prisma/client";
-import InventoryFilterPanel from "../../Panels/InventoryFilterPanel";
 
 interface InventoryDataTableProps {
   table: TableType<Inventory>;
@@ -41,6 +32,8 @@ const InventoryDataTable: React.FC<InventoryDataTableProps> = ({ table }) => {
     hidden: { y: 20, opacity: 0 },
     visible: { y: 0, opacity: 1 },
   };
+
+  const rows = table.getRowModel().rows;
 
   return (
     <>
@@ -71,29 +64,55 @@ const InventoryDataTable: React.FC<InventoryDataTableProps> = ({ table }) => {
             ))}
           </TableHeader>
           <TableBody>
-            <AnimatePresence>
-              {table.getRowModel().rows.map((row, index) => (
-                <motion.tr
-                  key={row.id}
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  variants={rowVariants}
-                  className={`border-b border-gray-200 hover:bg-green-100 cursor-pointer ${
-                    index % 2 === 0 ? "bg-gray-50" : "bg-white"
-                  }`}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </motion.tr>
-              ))}
-            </AnimatePresence>
+            {rows.length > 0 ? (
+              <AnimatePresence>
+                {rows.map((row, index) => (
+                  <motion.tr
+                    key={row.id}
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                    variants={rowVariants}
+                    className={`border-b border-gray-200 hover:bg-green-100 cursor-pointer ${
+                      index % 2 === 0 ? "bg-gray-50" : "bg-white"
+                    }`}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </motion.tr>
+                ))}
+              </AnimatePresence>
+            ) : (
+              // "Oops" message when no rows are found
+              <tr>
+                <td colSpan={table.getAllColumns().length}>
+                  <div className="flex flex-col items-center py-10">
+                    <div className="p-5 bg-gradient-to-b from-gray-200 to-white rounded-full">
+                      <GiEmptyMetalBucketHandle className="text-6xl text-gray-700" />
+                    </div>
+                    <div className="flex flex-col items-center mt-5">
+                      <p className="text-xl font-[600] text-[#101828]">
+                        Oops! Looks like we couldn't find anything
+                      </p>
+                      <p className="text-center font-[400] w-[450px] text-[16px] text-gray-600 mt-2">
+                        Hey, there looks like you dont have any product of this
+                        sort in your inventory maybe try{" "}
+                        <span className="underline decoration-gray-600">
+                          changing your filters or searching
+                        </span>
+                        , for something else.
+                      </p>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            )}
           </TableBody>
         </Table>
       </motion.div>
