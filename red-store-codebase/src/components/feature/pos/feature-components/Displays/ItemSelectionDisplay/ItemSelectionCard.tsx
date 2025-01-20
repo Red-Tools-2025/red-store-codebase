bd930e13f9903c2faf9b39719acb852f3fa8d214
akshatsabavat <akshat.sabavat@gmail.com>
2025-01-20 09:07:36 +0530

    Created: Basic Cart functionality and componatized the card

---
+++ red-store-codebase/src/components/feature/pos/feature-components/Displays/ItemSelectionDisplay/ItemSelectionCard.tsx
@@ -0,0 +1,52 @@
+import { Inventory } from "@prisma/client";
+import { useState } from "react";
+import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
+interface ItemSelectionCardProps {
+  item: Inventory;
+}
+
+const ItemSelectionCard: React.FC<ItemSelectionCardProps> = ({ item }) => {
+  const [cartCount, setCartCount] = useState<number>(0);
+
+  const handleCartCount = (sign: string) => {
+    if (sign === "negative") {
+      setCartCount((prev) => Math.max(prev - 1, 0));
+    } else {
+      setCartCount((prev) => Math.min(prev + 1, item.invItemStock));
+    }
+  };
+
+  return (
+    <div
+      key={item.invId}
+      className="bg-white border border-1 rounded-md p-3 flex flex-col h-40 justify-between"
+    >
+      <div className="flex text-xs gap-1">
+        <p>stock</p>
+        <IoIosArrowRoundForward />
+        <p>{item.invItemStock}</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-xs p-1 bg-gray-100 border border-1 w-fit">
+          {item.invItemBrand}
+        </p>
+        <p className="">{item.invItem}</p>
+      </div>
+      <div className="flex text-sm items-end items-center justify-end gap-3">
+        <CiCirclePlus
+          onClick={() => handleCartCount("positive")}
+          className="text-2xl hover:cursor-pointer hover:text-blue-500 transition-all"
+        />
+        <p className="">{cartCount}</p>
+        <CiCircleMinus
+          onClick={() => handleCartCount("negative")}
+          className="text-2xl hover:cursor-pointer hover:text-blue-500 transition-all"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ItemSelectionCard;
--- red-store-codebase/src/components/feature/pos/feature-components/Displays/ItemSelectionDisplay/index.tsx
+++ red-store-codebase/src/components/feature/pos/feature-components/Displays/ItemSelectionDisplay/index.tsx
@@ -1,6 +1,5 @@
 import { Inventory } from "@prisma/client";
-import { IoIosArrowRoundForward } from "react-icons/io";
-import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
+import ItemSelectionCard from "./ItemSelectionCard";
 
 interface ItemSelectionDisplayProps {
   isLoading: boolean;
@@ -18,27 +17,7 @@ const ItemSelectionDisplay: React.FC<ItemSelectionDisplayProps> = ({
       ) : (
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-4">
           {inventoryItems?.map((item) => (
-            <div
-              key={item.invId}
-              className="bg-white border border-1 rounded-md p-3 flex flex-col h-40 justify-between"
-            >
-              <div className="flex text-xs gap-1">
-                <p>stock</p>
-                <IoIosArrowRoundForward />
-                <p>{item.invItemStock}</p>
-              </div>
-              <div className="flex flex-col gap-2">
-                <p className="text-xs p-1 bg-gray-100 border border-1 w-fit">
-                  {item.invItemBrand}
-                </p>
-                <p className="">{item.invItem}</p>
-              </div>
-              <div className="flex text-sm items-end items-center justify-end gap-3">
-                <CiCirclePlus className="text-2xl hover:cursor-pointer hover:text-blue-500 transition-all" />
-                <p className="">0</p>
-                <CiCircleMinus className="text-2xl hover:cursor-pointer hover:text-blue-500 transition-all" />
-              </div>
-            </div>
+            <ItemSelectionCard item={item} />
           ))}
         </div>
       )}
