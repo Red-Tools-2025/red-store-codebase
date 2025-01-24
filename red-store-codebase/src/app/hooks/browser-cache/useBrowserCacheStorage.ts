af95469a056230f6fc900cf8bd18c32961cacd86
akshatsabavat <akshat.sabavat@gmail.com>
2025-01-25 01:20:55 +0530

    Edited cache save functionality

--- red-store-codebase/src/app/hooks/browser-cache/useBrowserCacheStorage.ts
+++ red-store-codebase/src/app/hooks/browser-cache/useBrowserCacheStorage.ts
@@ -1,5 +1,8 @@
+import { description } from "@/components/testing/TestLogin";
 import { Toast } from "@/components/ui/toast";
+import { useToast } from "@/hooks/use-toast";
 import { openDB, DBSchema } from "idb";
+import { title } from "process";
 import { Dispatch, SetStateAction } from "react";
 
 // Define the IndexedDB Schema
@@ -20,29 +23,40 @@ interface POSDbBuffer extends DBSchema {
 }
 
 const useBrowserCacheStorage = () => {
+  const { toast } = useToast();
   // Initializing the browser DB for object storage
   const initDB = openDB<POSDbBuffer>("pos-buffer", 1, {
     upgrade(db) {
       if (!db.objectStoreNames.contains("sales")) {
         const store = db.createObjectStore("sales", {
-          keyPath: "id",
+          keyPath: "cartItem.product_id",
         });
-        store.createIndex("storeId", "store_id");
-        store.createIndex("productId", "product_id");
+        store.createIndex("productId", "cartItem.product_id");
       }
     },
   });
 
   // Adding product to browser DB
   const saveToCache = async (
-    record: POSDbBuffer["sales"]["value"],
+    records: POSDbBuffer["sales"]["value"][],
     isSavingToCache: Dispatch<SetStateAction<boolean>>
   ) => {
     const db = await initDB;
     isSavingToCache(true);
-    await db.add("sales", record);
-    isSavingToCache(false);
-    console.log(`Product ${record.cartItem.product_id} saved to IndexedDB.`);
+    try {
+      for (const record of records) {
+        await db.add("sales", record); // Ensure each record has a unique `cartItem.product_id`
+      }
+      console.log(`${records.length} sales logged successfully.`);
+    } catch (error) {
+      console.error("Error saving to cache:", error);
+    } finally {
+      isSavingToCache(false);
+      toast({
+        title: "Saved to Buffer",
+        description: `${records.length} sales saved to buffer`,
+      });
+    }
   };
 
   return { saveToCache };
