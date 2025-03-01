00b2b7ac5842d9955df855241b435ad4e4f3bb18
Adi <adityashri09@gmail.com>
2025-03-02 03:17:15 +0530

    edit modified

--- red-store-codebase/src/components/feature/inventory/feature-component/FormModals/EditProductModal.tsx
+++ red-store-codebase/src/components/feature/inventory/feature-component/FormModals/EditProductModal.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import * as Yup from "yup";
-import React, { useEffect, useState } from "react";
+
+import React, {  useState } from "react";
 import { useFormik } from "formik";
 import {
   Dialog,
