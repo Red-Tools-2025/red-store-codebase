f0c7b74829fcb5574111aa47f8a2743fd13b8ff8
akshatsabavat <akshat.sabavat@gmail.com>
2025-06-28 22:14:49 +0530

    Defined: new nav header

--- red-store-codebase/src/components/feature/global/Header/index.tsx
+++ red-store-codebase/src/components/feature/global/Header/index.tsx
@@ -1,5 +1,62 @@
+import Image from "next/image";
+import LogoV1 from "@/public/logos/Logov1.svg";
+import { usePathname } from "next/navigation";
+import { IconType } from "react-icons/lib";
+import {
+  IoNotifications,
+  IoNotificationsOutline,
+  IoSettingsOutline,
+  IoStorefrontOutline,
+} from "react-icons/io5";
+import { MdPointOfSale } from "react-icons/md";
+import { FaDatabase } from "react-icons/fa";
+import { TbFileAnalytics } from "react-icons/tb";
+import React from "react";
+
+const NavigationLinks: { label: string; href: string; icon: IconType }[] = [
+  { label: "Dashboard", href: "/dashboard", icon: TbFileAnalytics },
+  { label: "Sales", href: "/sales", icon: MdPointOfSale },
+  { label: "Inventory", href: "/inventory", icon: FaDatabase },
+  { label: "Stores", href: "/management", icon: IoStorefrontOutline },
+];
+
 const Header = () => {
-  return <p className="p-3 max-w-[100%]">hi</p>;
+  const pathname = usePathname();
+  return (
+    <div className="py-4 border-b border-gray-300 px-10 max-w-[100%] flex items-center justify-between">
+      {/* Logo + search links */}
+      <div className="flex flex-row gap-6 items-center">
+        <Image src={LogoV1} alt="Logo" height={140} width={140} />
+        <div className="flex flex-row gap-5 items-center">
+          {NavigationLinks.map((link, i) => {
+            return (
+              <a
+                className={`text-[13px] font-medium text-gray-600 transition-all hover:text-black hover:font-semibold
+                    ${
+                      pathname.includes(link.href)
+                        ? "text-red-600 px-3 bg-red-100 rounded-md py-1"
+                        : ""
+                    }`}
+                key={i}
+                href={link.href}
+              >
+                <div className="flex flex-row gap-2 items-center">
+                  <> {React.createElement(link.icon)}</>
+
+                  <p>{link.label}</p>
+                </div>
+              </a>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center gap-5">
+        <IoSettingsOutline size={20} />
+        <IoNotificationsOutline size={20} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
